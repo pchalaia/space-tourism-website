a686aae9bfcb6cea29aea197db6eb979d424c055
pchalaia <polinachalaia@outlook.com>
2024-11-24 00:11:58 +0100

    .

---
+++ starter-code/index.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+// when someone clicks the hamburger button
+navToggle.addEventListener("click", () => {
+    
+    const visiblity = nav.getAttribute("data-visible");
+    // if the nav is closed, open it
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    // if the nav is opened, close it
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+    
+})
